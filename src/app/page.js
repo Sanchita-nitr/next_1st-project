bd49dcd37cd638d68bdc23e5cebc82165ab1b795
Sanchita-nitr <sanchitapriyadarshinee@gmail.com>
2024-06-01 22:26:07 +0530

    final commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,38 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import "./style.css";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default function home() {
+    return (
+        <>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide"></link>
+            <body>
+                <div className="page">
+                    <div className="body absolute text-2xl ">
+                        <p className=" about lg:text-5xl md:text-5xl text-teal-400 pt-5 pl-10 ">About us  </p>
+                        <div className="pt-10 border-b-2 border-teal-400"><hr /></div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                        <img className=" ho3 absolute lg:mx-60 lg:mt-64 lg:size-60 md:size-36 md:mx-36 md:mt-44 hidden md:block lg:block " src="https://www.hackodisha.tech/Images/HO3.svg" />
+                        <img className=" round absolute opacity-40 lg:mt-16 lg:mx-10 md:mx-10 md:mt-16 md:w-[350px] lg:w-[624px] hidden md:block lg:block " src="https://www.hackodisha.tech/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcircle-anim.fbd86c5f.gif&w=3840&q=75" />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                        <div className="flex justify-center item-center lg:hidden md:hidden ">
+                            <img className=" size-24  absolute mt-28 " src="https://www.hackodisha.tech/Images/HO3.svg" />
+                        </div>
+                        <div className="flex justify-center item-center lg:hidden md:hidden " >
+                            <img className="absolute w-[200px] opacity-40 mt-16 " src=" https://www.hackodisha.tech/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcircle-anim.fbd86c5f.gif&w=3840&q=75" />
+                        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                        <div className="building  ">
+                            <div className=" para relative text-xl text-justify justify-between lg:mx-24 lg:pl-72 md:mx-16 md:pl-28 lg:pt-28 md:pt-28 pt-80 mx-20">
+                                <p className="lg:ml-60 md:ml-60">HackOdisha- a thrilling <span class=" text-fuchsia-500">36-hour online hackathon </span>organized by <span class=" text-fuchsia-500 ">Webwiz, Nit Rourkela</span>— an event dedicated to fostering community collaboration. With an impressive turnout of<span class="text-fuchsia-500"> 3100 + participants in HackOdisha 2.0 </span> & <span class="text-fuchsia-500">1600 + participants in HackOdisha 1.0 </span>across India, this hackathon promises to be a platform where innovation knows no bounds.</p>
+                                <br />
+                                <p className="lg:ml-60 md:ml-60 "> Here, we witness groundbreaking projects spanning many fields, including healthcare, education, environment, women's security, and much more. Our participants bring their unique perspectives and expertise to address real-world challenges in these critical areas.</p>
+                                <br />
+                                <p className="lg:ml-60 md:ml-60">We celebrate the power of technology and the indomitable spirit of our participants. Together, we're shaping a brighter future through innovation and collaboration.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </body>
+        </>
+    );
 }
